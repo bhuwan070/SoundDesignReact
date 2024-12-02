e86d9c2b2d41127a794d74afb7532722087741a2
Biswash <koiralabiswash123@gmail.com>
2024-12-02 23:02:40 +0545

    My third commit

--- src/Components/Info/Info.jsx
+++ src/Components/Info/Info.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import "./Info.css"
+import {bgElement2, studentIcon, videoIcon} from "../../assets"
 
 const Info = () => {
   return (
-    <div>Info</div>
+    <section id='info' className='dark-gray'>
+      <div className="wrapper">
+        <div className="content-container">
+          <div className="info-content">
+            <img src={studentIcon} />
+            <div className="amount">23000+</div>
+            <div className="type">Students</div>
+          </div>
+
+          <div className="info-content">
+            <img src={videoIcon} />
+            <div className="amount">26 Hrs</div>
+            <div className="type">Video Content</div>
+          </div>
+        </div>
+
+        <img className="bg-element-2" src={bgElement2} />
+      </div>
+    </section>
   )
 }
 
