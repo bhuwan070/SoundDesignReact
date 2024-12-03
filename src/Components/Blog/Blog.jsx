b0b5be8145fa79f565c182c2ef67631a49e09710
Biswash <koiralabiswash123@gmail.com>
2024-12-03 21:39:51 +0545

    This is my fourth commit

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,9 +1,60 @@
 import React from 'react'
 import "./Blog.css"
+import { blog1, blog2, blog3 } from "../../assets"
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <section id='blog' className='pink'>
+      <div className="wrapper">
+        <h2 className="light">Latest Posts</h2>
+        <div className="content-container">
+          <div className="post">
+            <div className="tag">DAW</div>
+            <a href="#">
+              <img src={blog1} className='post-img'/>
+            </a>
+
+            <a href="#">
+              <h3 className="post-little">
+                How to use Drum Machine in Logic Pro X
+              </h3>
+            </a>
+          </div>
+
+          <div className="post">
+            <div className="tag">Mixing</div>
+            <a href="#">
+              <img src={blog2} className='post-img'/>
+            </a>
+
+            <a href="#">
+              <h3 className="post-little">
+                How to Mix Guitars Effectively
+              </h3>
+            </a>
+          </div>
+
+          <div className="post">
+            <div className="tag">Vox</div>
+            <a href="#">
+              <img src={blog3} className='post-img'/>
+            </a>
+
+            <a href="#">
+              <h3 className="post-little">
+                The Real Power of Harmonies  in Music Production
+              </h3>
+            </a>
+          </div>
+        </div>
+
+        <div className="btn-container">
+          <a href="#" className='all-posts-btn'>
+            All Posts
+          </a>
+        </div>
+      </div>
+    </section>
   )
 }
 
