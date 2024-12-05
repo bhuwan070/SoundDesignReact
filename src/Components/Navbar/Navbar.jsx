d2f8d1dcc6af2afc1971cffa96bc93ceec240aaf
Biswash <koiralabiswash123@gmail.com>
2024-12-05 12:07:00 +0545

    Responsive design commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,34 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.css"
-// import "../Info"
+import { menuIcon, closeIcon } from "../../assets"
 
 const Navbar = () => {
+
+  const [ isActive, setIsActive] = useState(false);
+
   return (
-    <nav>
-      <a href="#" className="logo">
-        Sound <span className='red'>DZign</span>
-      </a>
+    <>
+      <nav>
+        <a href="#" className="logo">
+          Sound <span className="red">DZign</span>
+        </a>
 
-      <ul>
-        <li>
-          <a href="#topics">Course Details</a>
-        </li>
+        <ul className="menu-items">
+          <li>
+            <a href="#topics">Course Details</a>
+          </li>
 
-        <li>
-          <a href="#info">About</a>
-        </li>
+          <li>
+            <a href="#info">About</a>
+          </li>
 
-        <li>
-          <a href="#blog">Blog</a>
-        </li>
+          <li>
+            <a href="#blog">Blog</a>
+          </li>
 
-        <li>
-          <a href="#testimonials">Testimonials</a>
-        </li>
+          <li>
+            <a href="#testimonials">Testimonials</a>
+          </li>
+        </ul>
 
-      </ul>
-    </nav>
-  )
+        <div onClick={() => setIsActive(true)} className="menu-icon">
+          <img src={menuIcon} />
+        </div>
+      </nav>
+
+      <div className={`mobile-menu-container ${ isActive ? "active" : "" }`}>
+        <div onClick={() => setIsActive(false)} className="close-icon">
+          <img src={closeIcon} alt="" />
+        </div>
+
+        <ul className="menu-items">
+          <li>
+            <a href="#topics">Course Details</a>
+          </li>
+
+          <li>
+            <a href="#info">About</a>
+          </li>
+
+          <li>
+            <a href="#blog">Blog</a>
+          </li>
+
+          <li>
+            <a href="#testimonials">Testimonials</a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default Navbar
